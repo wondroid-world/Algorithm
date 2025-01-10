23c817e177e12fc940886e5e1dce7767c974ad79
Hwang Chae Won(황 채원) <wondroid.world@gmail.com>
2025-01-10 12:17:31 +0900

    [Silver IV] Title: 스택, Time: 220 ms, Memory: 24512 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.kt
+++ 백준/Silver/10828. 스택/스택.kt
@@ -1,19 +1,33 @@
 fun main() {
-    val N = readln().toInt()
-    val numbers = mutableListOf<Int>()
-    val results = mutableListOf<Int>()
-    repeat(N) {
-        val word = readln()
-        when (word) {
-            "pop" -> if (numbers.size == 0) results.add(-1) else results.add(numbers.removeLast())
-            "size" -> results.add(numbers.size)
-            "empty" -> if (numbers.isEmpty()) results.add(1) else results.add(0)
-            "top" -> if (numbers.isEmpty()) results.add(-1) else results.add(numbers.last())
-            else -> {
-                val (push, number) = word.split(" ")
-                if (push == "push") numbers.add(number.toInt())
+    val n = readln().toInt()
+    val stack = mutableListOf<Int>()
+    val sb = StringBuilder()
+
+    repeat(n) {
+        val input = readln().split(" ")
+        when (input[0]) {
+            "push" -> {
+                // push X
+                stack.add(input[1].toInt())
+            }
+            "pop" -> {
+                // pop
+                if (stack.isEmpty()) sb.appendLine(-1)
+                else sb.appendLine(stack.removeLast())
+            }
+            "size" -> {
+                // size
+                sb.appendLine(stack.size)
+            }
+            "empty" -> {
+                // empty
+                sb.appendLine(if (stack.isEmpty()) 1 else 0)
+            }
+            "top" -> {
+                // top
+                sb.appendLine(if (stack.isEmpty()) -1 else stack.last())
             }
         }
     }
-    println(results.joinToString("\n"))
+    print(sb)
 }
