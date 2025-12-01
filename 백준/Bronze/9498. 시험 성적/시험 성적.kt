9f30396f4d19c6fb89e493cb14b43465b37b1a42
wondroid-world <wondroid.world@gmail.com>
2025-12-01 20:10:04 +0900

    [Bronze V] Title: 시험 성적, Time: 76 ms, Memory: 12380 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.kt
+++ 백준/Bronze/9498. 시험 성적/시험 성적.kt
@@ -1,11 +1,11 @@
 fun main() {
     val score = readln().toInt()
-    val grade = when (score) {
-        in 90..100 -> "A"
-        in 80..89 -> "B"
-        in 70..79 -> "C"
-        in 60..69 -> "D"
-        else -> "F"
+    val grade: Char = when (score) {
+        in 90..100 -> 'A'
+        in 80..89 -> 'B'
+        in 70..79 -> 'C'
+        in 60..69 -> 'D'
+        else -> 'F'
     }
     println(grade)
 }
