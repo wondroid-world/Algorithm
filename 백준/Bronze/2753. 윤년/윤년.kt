11de520a1fb90ff0f2aaf0389ccbf529328fe550
wondroid-world <wondroid.world@gmail.com>
2025-12-01 20:06:53 +0900

    [Bronze V] Title: 윤년, Time: 76 ms, Memory: 12336 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.kt
+++ 백준/Bronze/2753. 윤년/윤년.kt
@@ -1,5 +1,8 @@
 fun main() {
     val year = readln().toInt()
-    val result = if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) 1 else 0
-    println(result)
+    if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+        println(1)
+        return
+    }
+    println(0)
 }
