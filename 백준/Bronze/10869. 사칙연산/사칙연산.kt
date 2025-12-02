2402fcbf5eaadfb7e3ba1d50d9270d0673ca7f97
wondroid-world <wondroid.world@gmail.com>
2025-12-02 21:23:56 +0900

    [Bronze V] Title: 사칙연산, Time: 120 ms, Memory: 21044 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.kt
+++ 백준/Bronze/10869. 사칙연산/사칙연산.kt
@@ -1,5 +1,5 @@
 fun main() {
-    val (A, B) = readln().split(" ").map { it.toInt() }
+    val (A, B) = readln().split(" ").map(String::toInt)
     println(A + B)
     println(A - B)
     println(A * B)
