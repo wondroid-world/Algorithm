1bcb67bac25a11291f3a4b9b91baed248f190de7
wondroid-world <wondroid.world@gmail.com>
2026-01-02 11:40:26 +0900

    Title: 입력받은 값 교체하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260102/입력받은 값 교체하기/changing-inputs.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    
+    cin >> a >> b;
+
+    int temp = b;
+    b = a;
+    a = temp;
+
+    cout << a << " " << b;
+
+    return 0;
+}
