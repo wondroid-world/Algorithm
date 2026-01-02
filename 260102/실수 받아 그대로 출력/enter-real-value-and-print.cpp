333d65b078ee16278ed854b6a9258afd47aa86dc
wondroid-world <wondroid.world@gmail.com>
2026-01-02 11:28:35 +0900

    Title: 실수 받아 그대로 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260102/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double N;
+    
+    cin >> N;
+    
+    cout << fixed;
+    cout.precision(2);
+
+    cout << N;
+
+    return 0;
+}
