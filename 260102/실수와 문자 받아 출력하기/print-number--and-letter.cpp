eb5137dde5b078298f0f074fad60c25c49a90803
wondroid-world <wondroid.world@gmail.com>
2026-01-02 11:53:00 +0900

    Title: 실수와 문자 받아 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260102/실수와 문자 받아 출력하기/print-number--and-letter.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char c;
+    double a, b;
+
+    cin >> c >> a >> b;
+
+    cout << fixed;
+    cout.precision(2);
+
+    cout << c << endl << a << endl << b;
+    return 0;
+}
