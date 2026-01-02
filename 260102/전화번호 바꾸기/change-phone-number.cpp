2c40bc09db0ca1df46cf8daf919af2ff21dba691
wondroid-world <wondroid.world@gmail.com>
2026-01-02 12:15:11 +0900

    Title: 전화번호 바꾸기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260102/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    char dash;
+
+    cin >> a >> dash >> b >> dash >> c;
+    cout << "010" << dash << c << dash << b;
+    return 0;
+}
