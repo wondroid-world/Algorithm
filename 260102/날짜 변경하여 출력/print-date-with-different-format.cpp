be2b67e05a9adfac1408d1b2e2af3ee851756664
wondroid-world <wondroid.world@gmail.com>
2026-01-02 12:12:30 +0900

    Title: 날짜 변경하여 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260102/날짜 변경하여 출력/print-date-with-different-format.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int yyyy, mm, dd;
+    char dot;
+
+    cin >> yyyy >> dot >> mm >> dot >> dd;
+    cout << mm << "-" << dd << "-" << yyyy;
+    return 0;
+}
