b9abe89b2acb27d17e99fb1ca4894bf28ccc3926
wondroid-world <wondroid.world@gmail.com>
2026-01-03 19:00:23 +0900

    Title: 합과 평균의 차, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260103/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b, c;
+
+    cin >> a >> b >> c;
+    int sum = a + b + c;
+    int avg = sum / 3;
+
+    cout << sum << endl;
+    cout << avg << endl;
+    cout << sum - avg;
+    return 0;
+}
