144bfb487774f18b957c595a9816945ccbb0eab6
wondroid-world <wondroid.world@gmail.com>
2026-01-03 20:54:18 +0900

    Title: 삼항연산자, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260103/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int score;
+
+    cin >> score;
+    string result = score == 100 ? "pass" : "failure";
+    cout << result;
+    return 0;
+}
