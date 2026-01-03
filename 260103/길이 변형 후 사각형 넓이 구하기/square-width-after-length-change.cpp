1fdb6d8b715d118cc31676e6afc68a8b7d3a046a
wondroid-world <wondroid.world@gmail.com>
2026-01-03 18:14:56 +0900

    Title: 길이 변형 후 사각형 넓이 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260103/길이 변형 후 사각형 넓이 구하기/square-width-after-length-change.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cin >> a >> b;
+
+    a += 8;
+    b *= 3;
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << a * b;
+    
+    return 0;
+}
