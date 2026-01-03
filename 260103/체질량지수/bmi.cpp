3d19ff4108283519724835bd4b27e1fc7271944c
wondroid-world <wondroid.world@gmail.com>
2026-01-03 19:15:52 +0900

    Title: 체질량지수, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 260103/체질량지수/bmi.cpp
+++ 260103/체질량지수/bmi.cpp
@@ -5,15 +5,12 @@ int main() {
     // Please write your code here.
     int h, w;
     cin >> h >> w;
-    double b = (double)(10000 * w) / (h * h);
+    double b = (10000 * w) / (h * h);
 
-    cout << fixed;
-    cout.precision(1);
-
-    cout << b;
+    cout << b << endl;
     if (b > 25) {
         cout << "Obesity";
     } 
-    
+
     return 0;
 }
