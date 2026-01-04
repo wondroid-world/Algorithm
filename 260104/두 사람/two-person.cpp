65705da6301dd228fcdf5dd814af3c0fe00fc690
wondroid-world <wondroid.world@gmail.com>
2026-01-04 16:30:46 +0900

    Title: 두 사람, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260104/두 사람/two-person.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int a, c;
+    string b, d;
+    cin >> a >> b >> c >> d;
+
+    if ((a >= 19 && b == "M") || (c >= 19 && d == "M")) {
+        cout << 1;
+    } else {
+        cout << 0;
+    }
+    return 0;
+}
