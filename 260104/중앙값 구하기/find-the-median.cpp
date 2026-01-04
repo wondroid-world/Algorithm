276451874320a8f3bff0903283670f70ac586a70
wondroid-world <wondroid.world@gmail.com>
2026-01-04 17:37:07 +0900

    Title: 중앙값 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260104/중앙값 구하기/find-the-median.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if ((b < a && a <c) || (c < a && a <b)) {
+        cout << a;
+    } else if ((a < b && b <c) || (c < b && b <a)) {
+        cout << b;
+    } else {
+        cout << c;
+    }
+    return 0;
+}
