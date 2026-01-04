d0e09cd1686cf55a5d9a79f4be69bfbd6ad9e895
wondroid-world <wondroid.world@gmail.com>
2026-01-04 17:22:41 +0900

    Title: 최댓값 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260104/최댓값 구하기/maximum-value.cpp
+++ 260104/최댓값 구하기/maximum-value.cpp
@@ -4,11 +4,11 @@ using namespace std;
 int main() {
     int a, b, c;
     cin >> a >> b >> c;
-    if (a > b && a > c) {
+    if (a >= b && a >= c) {
         cout << a;
-    } else if (b > a && b > c) {
+    } else if (b >= a && b >= c) {
         cout << b;
-    } else if (c > a && c > b) {
+    } else if (c >= a && c >= b) {
         cout << c;
     }
     return 0;
