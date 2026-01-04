515a2d0b7a54b406c6ef16b8ccd08564a1ebc5d6
wondroid-world <wondroid.world@gmail.com>
2026-01-04 15:43:02 +0900

    Title: 범위 안의 수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260104/범위 안의 수/number-in-range.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    if (10 <= a && a <= 20) {
+        cout << "yes";
+    } else {
+        cout << "no";
+    }
+    return 0;
+}
