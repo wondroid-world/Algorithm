f3f6b27f7f82958d75913037b899a89b16e9c5f1
wondroid-world <wondroid.world@gmail.com>
2026-01-04 15:52:00 +0900

    Title: 세 수의 중간값, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260104/세 수의 중간값/median-of-three-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a < b && b < c) {
+        cout << 1;
+    } else {
+        cout << 0;
+    }
+    return 0;
+}
