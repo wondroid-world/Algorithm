55efdf14d0a765301a4b987e536eb6ae2d2e0f61
wondroid-world <wondroid.world@gmail.com>
2026-01-06 13:18:03 +0900

    Title: N까지 자연수 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260106/N까지 자연수 출력/print-1-to-n.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    int i = 1;
+
+    cin >> a;
+
+    while (i <= a) {
+        cout << i << " ";
+        i++;
+    }
+    return 0;
+}
