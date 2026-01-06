73dc2e7e630dc75157636909c503e6c2524723ca
wondroid-world <wondroid.world@gmail.com>
2026-01-06 13:19:41 +0900

    Title: 별 N개 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260106/별 N개 출력하기/print-n-stars.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    int i = 1;
+
+    cin >> N;
+
+    while (i <= N) {
+        cout << "*\n";
+        i++;
+    }
+    return 0;
+}
