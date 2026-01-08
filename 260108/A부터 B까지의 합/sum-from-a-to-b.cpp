960cd35aefca7748d340324730d30c9bf13fec93
wondroid-world <wondroid.world@gmail.com>
2026-01-08 18:57:11 +0900

    Title: A부터 B까지의 합, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260108/A부터 B까지의 합/sum-from-a-to-b.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int cnt = 0;
+    while (a <= b) {
+        cnt += a++;
+    }
+    cout << cnt;
+    return 0;
+}
