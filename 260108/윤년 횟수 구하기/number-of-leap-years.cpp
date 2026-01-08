0f47b346d568e19b290b660c2679f9d3855d81d1
wondroid-world <wondroid.world@gmail.com>
2026-01-08 18:52:37 +0900

    Title: 윤년 횟수 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260108/윤년 횟수 구하기/number-of-leap-years.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int cnt = 0;
+    for (int i = 1; i <= n; i++) {
+        if (i % 4 == 0 && !(i % 100 == 0 && i % 400 != 0)) {
+            cnt++;
+        }
+    }
+    cout << cnt;
+    return 0;
+}
