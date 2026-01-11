a5a5c6c253773be657702071e70be4570378fe9b
wondroid-world <wondroid.world@gmail.com>
2026-01-11 21:38:41 +0900

    Title: A의 배수들의 곱, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260111/A의 배수들의 곱/product-of-multiples-of-a.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    int prod = 1;
+
+    cin >> a >> b;
+
+    for (int i = 1; i <= b; i++) {
+        if (i % a == 0) {
+            prod *= i;
+        }
+    }
+
+    cout << prod;
+    
+    return 0;
+}
