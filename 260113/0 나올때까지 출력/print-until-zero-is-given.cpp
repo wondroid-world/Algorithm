049cf13c66585b5cb96ab6d2c083b6e8498478e9
wondroid-world <wondroid.world@gmail.com>
2026-01-13 08:56:37 +0900

    Title: 0 나올때까지 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260113/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    for (;;) {
+        cin >> num;
+        if (num == 0) break;
+        cout << num << endl;
+    }
+    return 0;
+}
