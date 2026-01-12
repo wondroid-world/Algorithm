39241c3ec21067d8deb05c3c40f3f32edb46c03e
wondroid-world <wondroid.world@gmail.com>
2026-01-12 10:15:31 +0900

    Title: 친근하지 않은 수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260112/친근하지 않은 수/unfriendly-number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    int cnt = 0;
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 == 0 || i % 3 == 0 || i % 5 == 0) {
+            continue;
+        }
+        cnt++;
+    }
+
+    cout << cnt;
+    return 0;
+}
