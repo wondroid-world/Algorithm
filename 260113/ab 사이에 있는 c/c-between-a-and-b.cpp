ba203c19b1facba30cf46cf5062d89a67a2654eb
wondroid-world <wondroid.world@gmail.com>
2026-01-13 09:44:38 +0900

    Title: ab 사이에 있는 c, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260113/ab 사이에 있는 c/c-between-a-and-b.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    bool satisfied = false;
+    for (int i = a; i <= b; i++) {
+        if (i % c == 0) {
+            satisfied = true;
+            break;
+        }
+    }
+    
+    if (satisfied) {
+        cout << "YES";
+    } else {
+cout << "NO";
+    }
+    
+    return 0;
+}
