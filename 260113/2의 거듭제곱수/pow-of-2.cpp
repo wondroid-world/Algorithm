2ea82cd0625bbed322f7ec63f4e5b141793e66c4
wondroid-world <wondroid.world@gmail.com>
2026-01-13 09:37:21 +0900

    Title: 2의 거듭제곱수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260113/2의 거듭제곱수/pow-of-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int cnt = 0;
+    cin >> n;
+    while (n != 1) {
+        n /= 2;
+        cnt++;
+    }
+    cout << cnt;
+    return 0;
+}
