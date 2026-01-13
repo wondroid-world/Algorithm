6c0210ce2454f4d9f0bd45b3023ee81822f88010
wondroid-world <wondroid.world@gmail.com>
2026-01-13 09:05:18 +0900

    Title: 출석 부르기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260113/출석 부르기 2/calling-attendance-2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+
+    while (true) {
+        cin >> num;
+        if (num == 1) {
+            cout << "John" << endl;
+        } else if (num == 2) {
+            cout << "Tom" << endl;
+        } else if (num == 3) {
+            cout << "Paul" << endl;
+        } else if (num == 4) {
+            cout << "Sam" << endl;
+        } else {
+            cout << "Vacancy" << endl;
+            break;
+        }
+    }
+    return 0;
+}
