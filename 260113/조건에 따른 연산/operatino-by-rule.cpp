16df3a6104d33dfcc7a72bd805f0106ce9411271
wondroid-world <wondroid.world@gmail.com>
2026-01-13 09:39:41 +0900

    Title: 조건에 따른 연산, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260113/조건에 따른 연산/operatino-by-rule.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int cnt = 0;
+    cin >> n;
+    while (n < 1000) {
+        if (n % 2 == 0) {
+            n = n * 3 + 1;
+        } else {
+            n = n * 2 + 2;
+        }
+        cnt++;
+    }
+
+    cout << cnt;
+    return 0;
+}
