a8705230af11c60ab12a02b8103c25d100efedf7
wondroid-world <wondroid.world@gmail.com>
2026-01-15 15:26:30 +0900

    Title: 구구단 만들기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260115/구구단 만들기/print-multiplication-table.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    for (int i = 1; i <= 9; i++) {
+        for (int j = b;j >= a; j -= 2) {
+            cout << j << " * " << i << " = " << i * j;
+            if (j != a) {
+                cout << " / ";
+            }
+        }
+        cout << endl;        
+    }
+    return 0;
+}
