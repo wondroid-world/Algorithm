8a74518d377b756256219675e621c16694e22e7c
wondroid-world <wondroid.world@gmail.com>
2026-01-18 10:00:35 +0900

    Title: 배열 채우고 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260118/배열 채우고 출력하기/filling-array-and-print.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char arr[10];
+
+    for (int i = 0; i < 10; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < 10; i++) {
+        cout << arr[9 - i];
+    }    
+
+    return 0;
+}
