da8e684b973defea7ed4e44eaf5fcf05a2b0a747
wondroid-world <wondroid.world@gmail.com>
2026-01-18 12:12:24 +0900

    Title: 배열에서 원소의 특징, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260118/배열에서 원소의 특징/characteristics-of-elements-in-an-array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10];
+
+    for (int i = 0; i < 10; i++) {
+        cin >> arr[i];
+        if (arr[i] % 3 == 0) {
+            cout << arr[i - 1];
+            break;
+        }
+    }
+    return 0;
+}
