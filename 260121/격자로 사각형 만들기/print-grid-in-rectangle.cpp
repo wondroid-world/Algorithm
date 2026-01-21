9745175ac1c924f313bd5a73f615b391eb0d680f
wondroid-world <wondroid.world@gmail.com>
2026-01-21 16:32:22 +0900

    Title: 격자로 사각형 만들기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260121/격자로 사각형 만들기/print-grid-in-rectangle.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n][n] = {};
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= n; j++) {
+            if (j == 0 || i == 0) {
+                arr[i][j] = 1;
+            }
+        }
+    }
+
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j < n; j++) {
+            arr[i][j] = arr[i-1][j - 1] + arr[i - 1][j] + arr[i][j - 1];
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << arr[i][j] << ' ';
+        }
+        cout << endl;
+    }
+    return 0;
+}
