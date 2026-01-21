7811e48c674a4fc6ef2357e3cfc78503fb74c9eb
wondroid-world <wondroid.world@gmail.com>
2026-01-21 17:18:19 +0900

    Title: 동전이 있는 위치, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260121/동전이 있는 위치/Where-coins-are-located.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, r, c;
+    cin >> n >> m;
+    int arr[n][n] = {0};
+
+    for (int i = 0; i < m; i++) {
+        cin >> r >> c;
+        arr[r][c] = 1;
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << arr[i][j] << ' ';
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
