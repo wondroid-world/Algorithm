1d1d7ec8d69344b5b319ee507929d9293fb702aa
wondroid-world <wondroid.world@gmail.com>
2026-02-05 15:37:53 +0900

    Title: 문자 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260205/문자 출력하기/print-text.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char word;
+    cin >> word;
+    for (int i =0; i <8;i++){
+        cout << word;
+    }
+    return 0;
+}
