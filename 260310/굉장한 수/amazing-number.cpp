1cf6ec7391c8e75a84488cc1a2409f57c3468cea
wondroid-world <wondroid.world@gmail.com>
2026-03-10 09:01:46 +0900

    Title: 굉장한 수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260310/굉장한 수/amazing-number.cpp
+++ 260310/굉장한 수/amazing-number.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    if ((n % 2 == 1 && n % 3 == 0) || (n % 2 == 0 && n % 7 == 0)) {
+    if ((n % 2 == 1 && n % 3 == 0) || (n % 2 == 0 && n % 5 == 0)) {
         cout << "true";
     } else {
         cout << "false";
